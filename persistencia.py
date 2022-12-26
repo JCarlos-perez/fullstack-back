6fb81d33b1294f796bf0ac09085cca9e961efd36
JCarlos-perez <120038083+JCarlos-perez@users.noreply.github.com>
2022-12-26 19:11:24 +0100

    Changes to pass pytest

--- persistencia.py
+++ persistencia.py
@@ -4,5 +4,5 @@ def guardar_pedido(nombre, apellidos):
     """Procedure guardar_pedido to save data in a file"""
     usuario = {"nom":nombre, "ape":apellidos}
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write (usuario["nom"] + " " + usuario["ape"] + "\n")
+        file.write ("-" + usuario["nom"] + " " + usuario["ape"] + "\n")
         file.close()
