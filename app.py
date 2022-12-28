fa15a6031bb19fa436be75bb00dd2f2597071d1a
JCarlos-perez <120038083+JCarlos-perez@users.noreply.github.com>
2022-12-28 19:42:03 +0100

    Corrección errores pipeline backend

--- app.py
+++ app.py
@@ -20,20 +20,21 @@ def pizza():
     print("Nombre Completo: " + v_nombre + " " + v_apellidos)
     guardar_pedido(v_nombre, v_apellidos)
     return redirect("http://localhost/solicita_pedido.html", code=302)
-  
+
+
 @app.route("/checksize",methods=['POST'])
 def checksize():
-  """
-  Comprueba disponibilidad de un tamaño de pizza.
-  """
-  # Aquí va el código Python. Debe capturar el parámetro "size" de la request. Debe
-  # retornar siempre "Disponible", excepto para el tamaño "S" que debe retornar "No
-  # disponible" y se debe asignar en mensaje, así mensaje = "Lo que corresponda"
-  
-  v_size = request.form.get("size")
-  if v_size == "S": 
-    mensaje = "No Disponible"
-  else:
-    mensaje = "Disponible"
-  
-  return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
+    """
+    Comprueba disponibilidad de un tamaño de pizza.
+    """
+    # Aquí va el código Python. Debe capturar el parámetro "size" de la request. Debe
+    # retornar siempre "Disponible", excepto para el tamaño "S" que debe retornar "No
+    # disponible" y se debe asignar en mensaje, así mensaje = "Lo que corresponda"
+
+    v_size = request.form.get("size")
+    if v_size == "S":
+        mensaje = "No Disponible"
+    else:
+        mensaje = "Disponible"
+
+    return Response(mensaje, 200, {'Access-Control-Allow-Origin': '*'})
